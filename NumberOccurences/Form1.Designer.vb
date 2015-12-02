1fa41c58776c3d4b9f672f4357e7d3f613dbd271
pxhanus <pxhanus@gmail.com>
2015-12-02 13:38:36 -0600

    Initial commit

---
+++ NumberOccurences/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNum = New System.Windows.Forms.TextBox()
+        Me.lstNumOccurences = New System.Windows.Forms.ListBox()
+        Me.btnCountNumerals = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a number:"
+        '
+        'txtNum
+        '
+        Me.txtNum.Location = New System.Drawing.Point(100, 28)
+        Me.txtNum.Name = "txtNum"
+        Me.txtNum.Size = New System.Drawing.Size(155, 20)
+        Me.txtNum.TabIndex = 1
+        '
+        'lstNumOccurences
+        '
+        Me.lstNumOccurences.FormattingEnabled = True
+        Me.lstNumOccurences.Location = New System.Drawing.Point(15, 77)
+        Me.lstNumOccurences.Name = "lstNumOccurences"
+        Me.lstNumOccurences.Size = New System.Drawing.Size(120, 134)
+        Me.lstNumOccurences.TabIndex = 2
+        '
+        'btnCountNumerals
+        '
+        Me.btnCountNumerals.Location = New System.Drawing.Point(162, 77)
+        Me.btnCountNumerals.Name = "btnCountNumerals"
+        Me.btnCountNumerals.Size = New System.Drawing.Size(93, 27)
+        Me.btnCountNumerals.TabIndex = 3
+        Me.btnCountNumerals.Text = "Count Numerals"
+        Me.btnCountNumerals.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(267, 223)
+        Me.Controls.Add(Me.btnCountNumerals)
+        Me.Controls.Add(Me.lstNumOccurences)
+        Me.Controls.Add(Me.txtNum)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Number Occurences"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNum As System.Windows.Forms.TextBox
+    Friend WithEvents lstNumOccurences As System.Windows.Forms.ListBox
+    Friend WithEvents btnCountNumerals As System.Windows.Forms.Button
+
+End Class
---
+++ NumberOccurences/Form1.vb
@@ -0,0 +1,47 @@
+﻿'Patricia Hanus
+'151202
+'Number Occurences
+'_____________________________________________________________________________
+Public Class Form1
+
+    Private Sub btnCountNumerals_Click(sender As Object, e As EventArgs) Handles btnCountNumerals.Click
+        Dim numStr As String = Me.txtNum.Text
+        Dim occurences(9) As Integer
+
+        Call NumOccur(numStr, occurences)
+        Call DisplayOccur(Me.lstNumOccurences, occurences)
+    End Sub
+    '********************************************************************************************
+    'NumOccur
+    'Counts the occurences of numbers in a string
+    'pre:  An appropriately sized array - size 10	
+    'post: returns a filled array 
+    '********************************************************************************************
+    Sub NumOccur(ByVal numStr As String, ByRef array() As Integer)
+        Dim numIndex As String
+        Dim num As Char
+        For index As Integer = 0 To numStr.Length - 1
+            num = numStr.Chars(index)
+            If num >= ChrW(48) _
+                And num <= ChrW(57) Then
+                numIndex = (AscW(num) - 48)
+                array(numIndex) += 1
+            End If
+        Next
+    End Sub
+    '********************************************************************************************
+    'DisplayOccur
+    'Displays the contents of the passed array in the passed list box values index
+    '
+    'post:  Array results displayed in list box 
+    '********************************************************************************************
+    Sub DisplayOccur(ByRef lstbox As ListBox, ByRef array() As Integer)
+        For index As Integer = 0 To array.Length - 1
+            lstbox.Items.Add(index & vbTab & array(index))
+        Next
+    End Sub
+
+    Private Sub txtNum_TextChanged(sender As Object, e As EventArgs) Handles txtNum.TextChanged
+        Me.lstNumOccurences.Items.Clear()
+    End Sub
+End Class
